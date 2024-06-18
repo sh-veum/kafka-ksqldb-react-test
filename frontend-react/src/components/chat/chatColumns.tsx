830a20639ed8768406a5732c473e0b97e9850eac
Veum <sh.veum@gmail.com>
2024-06-18 04:07:39 +0200

    Fixed Chat table

--- frontend-react/src/components/chat/chatColumns.tsx
+++ frontend-react/src/components/chat/chatColumns.tsx
@@ -6,35 +6,9 @@ export const chatColumns: ColumnDef<ChatMessage>[] = [
   {
     accessorKey: "Username",
     header: "Username",
-    // cell: ({ row }) => {
-    //   const username = row.getValue("Username") as string;
-    //   const timestamp = row.getValue("Created_Timestamp") as string;
-    //   const date = parseISO(timestamp);
-
-    //   let formattedDate;
-    //   if (isToday(date)) {
-    //     formattedDate = format(date, "HH:mm:ss");
-    //   } else {
-    //     formattedDate = format(date, "MMMM d, yyyy 'at' HH:mm");
-    //   }
-    //   return (
-    //     <div>
-    //       <div className="text-xs italic color">{formattedDate}</div>
-    //       <div className="text-base">{username}</div>
-    //     </div>
-    //   );
-    // },
-    // sortingFn: "datetime",
-  },
-  {
-    accessorKey: "Message_Text",
-    header: "Message",
-  },
-  {
-    accessorKey: "Created_Timestamp",
-    header: "Created Timestamp",
     cell: ({ row }) => {
-      const timestamp = row.getValue("Created_Timestamp") as string;
+      const username = row.getValue("Username") as string;
+      const timestamp = row.original.Created_Timestamp as string;
       const date = parseISO(timestamp);
 
       let formattedDate;
@@ -43,11 +17,27 @@ export const chatColumns: ColumnDef<ChatMessage>[] = [
       } else {
         formattedDate = format(date, "MMMM d, yyyy 'at' HH:mm");
       }
-      return <div>{formattedDate}</div>;
+      return (
+        <div>
+          <div className="text-xs italic color">{formattedDate}</div>
+          <div className="text-base">{username}</div>
+        </div>
+      );
     },
+    sortingFn: "datetime",
   },
   {
-    accessorKey: "Is_Edited",
-    header: "Edited",
+    accessorKey: "Message_Text",
+    header: "Message",
+    cell: ({ row }) => {
+      const message = row.getValue("Message_Text") as string;
+      const isEdited = row.original.Is_Edited as boolean;
+
+      return (
+        <div>
+          {message} {isEdited && <span className="italic">(edited)</span>}
+        </div>
+      );
+    },
   },
 ];
